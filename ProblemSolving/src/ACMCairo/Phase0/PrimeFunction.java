8cba7dd42ed1e356f4ddd28a8d3fa4e5fec2f1d3
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2021-01-31 13:38:44 +0200

    update

--- ProblemSolving/src/ACMCairo/Phase0/PrimeFunction.java
+++ ProblemSolving/src/ACMCairo/Phase0/PrimeFunction.java
@@ -16,7 +16,7 @@ public class PrimeFunction {
     }
     public static boolean isprime(int n) {
 if(n==1)return false;
-        for (int i = 2; i <= n/2; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
 
         }
