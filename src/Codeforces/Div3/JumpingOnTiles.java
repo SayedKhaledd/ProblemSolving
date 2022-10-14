7f30c8d361187e8a86efb30db9208663b3defa85
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-10-14 02:35:25 +0200

    update

---
+++ src/Codeforces/Div3/JumpingOnTiles.java
@@ -0,0 +1,16 @@
+package Codeforces.Div3;
+//https://codeforces.com/contest/1729/problem/C
+
+import java.util.Scanner;
+
+public class JumpingOnTiles {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            String s= scan.next();
+
+        }
+    }
+
+}
