a7c9eb23d9c55d89cf23614a96044b1775575f61
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2022-12-12 20:23:54 +0200

    update

--- src/Codeforces/Div2/ExtremelyRound.java
+++ src/Codeforces/Div2/ExtremelyRound.java
@@ -6,5 +6,15 @@ import java.util.Scanner;
 public class ExtremelyRound {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            if (n <= 10) {
+                System.out.println(n);
+            } else {
+                String s=n+"";
+                System.out.println(10+(s.length()-2)*9+s.charAt(0)-48-1);
+            }
+        }
     }
 }
