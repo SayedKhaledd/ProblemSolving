d2aea153bfbe35b68d6257db1e009408d9af4286
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-02-02 22:42:55 +0200

    update

--- src/Test.java
+++ src/Test.java
@@ -2,148 +2,116 @@ import java.io.*;
 import java.util.*;
 
 public class Test {
-    private static class MyScanner {
-        static StringTokenizer st;
-        static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        static PrintWriter pt = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
-        public static String next() throws IOException {
-            while (st == null || !st.hasMoreTokens())
-                st = new StringTokenizer(reader.readLine().trim());
-            return st.nextToken();
-        }
 
-        public static int readInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-
-        public static long readLong() throws IOException {
-            return Long.parseLong(next());
-        }
-
-        public static double readDouble() throws IOException {
-            return Double.parseDouble(next());
-        }
-
-        public static char readChar() throws IOException {
-            return next().charAt(0);
-        }
-
-        public static String readLine() throws IOException {
-            return reader.readLine().trim();
-        }
-    }
+    static int[][] left = new int[2][3];
+    static int[][] middle_left = new int[3][4];
+    static int[][] middle_right = new int[3][2];
+    static int[][] right = new int[4][3];
+    static int n = 30, counter = 0;
 
     public static void main(String[] args) throws IOException {
-        MyScanner scan = new MyScanner();
-        int t = scan.readInt();
-        while (t-- > 0) {
-            long n = scan.readLong();
-            int x = getDivisors(n).size();
-            if (x == 3)
-                System.out.println("YES");
+        //   int n = MyScanner.readInt();
+
+        //TODO: aisle seats
+        final int MAX_ROWS = Math.max(left.length, Math.max(middle_left.length, Math.max(middle_right.length, right.length)));
+        for (int i = 0; i < MAX_ROWS; i++) {
+            //the left seats
+            if (i < left.length && counter < n)
+                left[i][left[i].length - 1] = ++counter;
+
+            //the middle left seats
+            if (i < middle_left.length && counter < n) {
+                middle_left[i][0] = ++counter;
+            }
+            if (i < middle_left.length && counter < n) {
+                System.out.println(i);
+                middle_left[i][middle_left[i].length - 1] = ++counter;
+            }
+
+            //the middle right seats
+            if (i < middle_right.length && counter < n) {
+                middle_right[i][0] = ++counter;
+            }
+            if (i < middle_right.length && counter < n) {
+                middle_right[i][middle_right[i].length - 1] = ++counter;
+            }
+
+            //the right seats
+            if (i < right.length && counter < n)
+                right[i][0] = ++counter;
+
+
+        }
+
+        //TODO: window seats
+        final int MAX_WINDOW_ROWS = Math.max(left.length, right.length);
+        for (int i = 0; i < MAX_WINDOW_ROWS; i++) {
+            if (i < left.length && counter < n)
+                left[i][0] = ++counter;
+            if (i < right.length && counter < n)
+                right[i][right[i].length - 1] = ++counter;
+
+        }
+
+        //TODO: middle seats
+        for (int i = 0; i < MAX_ROWS; i++) {
+            //left seats
+            if (i < left.length)
+                printMiddleSeats(left, i);
+
+            //middle left seats
+            if (i < middle_left.length)
+                printMiddleSeats(middle_left, i);
+
+            //middle right seats
+            if (i < middle_right.length)
+                printMiddleSeats(middle_right, i);
+            //right seats
+            if (i < right.length)
+                printMiddleSeats(right, i);
+
+
+        }
+
+
+        printSeats();
+
+
+    }
+
+    public static void printMiddleSeats(int[][] arr, int i) {
+        for (int j = 1; j < arr[i].length - 1 && counter < n; j++) {
+            if (arr[i][j] != 0)
+                break;
             else
-                System.out.println("NO");
-
+                arr[i][j] = ++counter;
         }
+    }
+
+    public static void printSeats() {
+        System.out.println("left seats");
+        print2DArray(left);
+
+        System.out.println("middle left seats");
+        print2DArray(middle_left);
+
+        System.out.println("middle right seats");
+        print2DArray(middle_right);
+
+        System.out.println("right seats");
+        print2DArray(right);
 
     }
 
-    public static List<Long> getDivisors(long n) {
-        ArrayList<Long> arr = new ArrayList<>();
-        if (n == 1) {
-            arr.add(1l);
-            return arr;
-        }
-        while (n % 2 == 0) {
-            n /= 2;
-            arr.add(2l);
-            if (arr.size() >= 3)
-                break;
-        }
-        for (long i = 3; i <= Math.sqrt(n); i += 2) {
-            if (arr.size() >= 3)
-                break;
-            while (n % i == 0) {
-                n /= i;
-                arr.add(i);
-                if (arr.size() >= 3)
-                    break;
+    public static void print2DArray(int[][] arr) {
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
-            if (arr.size() >= 3)
-                break;
-
+            System.out.println();
         }
-        if (n > 2)
-            arr.add(n);
-        if (n == 1)
-            arr.add(1l);
-        return arr;
+
     }
 
-}
-
-class Solution {
-    public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-//        int n= scan.nextInt();
-        String[] strs={"a","b","a","a"};
-//        for(int i=0;i<n;i++)
-//            strs[i]= scan.next();
-//        ArrayList<String> in= new ArrayList<>();
-//        Collections.addAll(in, strs);
-//        in.remove(2);
-//        System.out.println(in);
-        System.out.println(groupAnagrams(strs));
-    }
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        ArrayList<List<String>> arrs = new ArrayList<>();
-        ArrayList<String> in= new ArrayList<>();
-        Collections.addAll(in, strs);
-
-        for (int i = 0; i < in.size(); i++) {
-            arrs.add(new ArrayList<>());
-            arrs.get(i).add(in.get(i));
-            System.out.println("the input is like that");
-            System.out.println(in);
-            System.out.println("i is "+ i+ " and the value is "+in.get(i));
-
-            for(int j=i+1;j<in.size();j++){
-                if(isAnagram(in.get(i),in.get(j))){
-                    arrs.get(i).add(in.get(j));
-                    System.out.println("remove "+ in.get(j)+" in position "+j);
-                   in.remove(in.lastIndexOf(in.get(j)));
-                    j--;
-                }
-
-            }
-            System.out.println("now the input is like that");
-            System.out.println(in);
-            System.out.println("and the output is like that");
-
-            System.out.println(arrs);
-        }
-        return arrs;
-    }
-
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        char[] freq = new char[26];
-        char[] freq2 = new char[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 97]++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            freq2[t.charAt(i) - 97]++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != freq2[i])
-                return false;
-        }
-        return true;
-
-
-    }
 }
