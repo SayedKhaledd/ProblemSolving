ceb4de34156e9be4bfd246000d585ec2a73d02ac
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-02-04 23:24:48 +0200

    update

---
+++ src/Codeforces/Div4/PrependAndAppend.java
@@ -0,0 +1,26 @@
+package Codeforces.Div4;
+
+import java.util.Scanner;
+
+//https://codeforces.com/contest/1791/problem/C
+public class PrependAndAppend {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            String s = scan.next();
+            int counter = n;
+            for (int i = 0, j = n - 1; i < n / 2; i++, j--) {
+                if (i == j)
+                    break;
+                else if (s.charAt(i) == s.charAt(j))
+                    break;
+                else {
+                    counter -= 2;
+                }
+            }
+            System.out.println(counter);
+        }
+    }
+}
