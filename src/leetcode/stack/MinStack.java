edb665a882ea88373f938f65dcfa0bbbcdc85bb1
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-03-12 22:13:54 +0200

    update

---
+++ src/leetcode/stack/MinStack.java
@@ -0,0 +1,35 @@
+package leetcode.stack;
+
+import java.util.ArrayList;
+
+public class MinStack {
+
+    ArrayList<Integer> stack;
+
+    public MinStack() {
+        stack = new ArrayList<>();
+    }
+
+    public void push(int val) {
+        stack.add(val);
+    }
+
+    public void pop() {
+        stack.remove(stack.size() - 1);
+    }
+
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public int getMin() {
+        int min = Integer.MAX_VALUE;
+        for (int i : stack) {
+            if (i < min)
+                min = i;
+        }
+        return min;
+    }
+
+}
+
